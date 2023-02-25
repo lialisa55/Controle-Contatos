19bb397ef249446bad01d10c35d9a472929cb87e
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-02-25 18:57:48 -0300

    NetCore libs installation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CadastroContatos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var connectionString = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>(options =>
+     options.UseSqlServer(connectionString));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
